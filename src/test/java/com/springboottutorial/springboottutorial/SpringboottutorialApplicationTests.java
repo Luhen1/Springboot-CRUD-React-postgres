d3e7797f7299e99a3b7a3235f53956d2ec94577e
Lucas <lucashbsj@outlook.com>
2021-10-09 17:18:40 -0300

    first commit

---
+++ src/test/java/com/springboottutorial/springboottutorial/SpringboottutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboottutorial.springboottutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboottutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
